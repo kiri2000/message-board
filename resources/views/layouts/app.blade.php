03424e44dba445bb7c69678185366195748d6b81
kiri <kiri2000h12@gmail.com>
2019-09-16 15:56:24 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,15 @@
         @include('commons.navbar')
         
         <div class="container">
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
         
-        <!-- JavaScriptの指定は省略 -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+ 
     </body>
 </html>
